27f78367fdae4dd284d5be8c879854c245ccd01d
Naman Katewa <85558238+NamanKatewa@users.noreply.github.com>
2025-06-15 01:20:49 +0530

    style: Body Margin for Navbar Accomodation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${geist.variable}`}>
-      <body className="bg-blue-50">
+      <body className="bg-blue-50 mt-16">
         <TRPCReactProvider>
           <Navbar />
           {children}
