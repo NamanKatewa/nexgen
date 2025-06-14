7f42be6cce9c34ecda9fd5d1a9e087b7bc187368
Naman Katewa <85558238+NamanKatewa@users.noreply.github.com>
2025-06-14 20:40:31 +0530

    chore: Cleaning

--- src/schemas/wallet.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const rechargeWalletSchema = z.object({
-  user_id: z.string().uuid(),
-  amount: z.number().min(1),
-});
-
-export type RechargeWalletInput = z.infer<typeof rechargeWalletSchema>;
